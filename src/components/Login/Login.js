0adfe06ba59b95b6c6a52ef72ecad7d21ee87238
minhnguyet2308 <nguyetnmse172621@fpt.edu.vn>
2024-07-08 16:11:26 +0700

    fix login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,48 +3,50 @@ import "./Login.css";
 import { Helmet } from "react-helmet";
 import LogoWhite from "../../assets/images/Logo-white.png";
 import axios from "axios";
+// import { toast } from "react-toastify";
 
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  // const [error, setError] = useState("");
 
   const handleSubmit = async () => {
     // e.preventDefault();
+    alert("Login successfully");
 
-    try {
-      const loginAPI = await axios.post(
-        "http://localhost:5236/api/Authentication/Login",
-        {
-          email,
-          password,
-        }
-      );
+    // try {
+    const loginAPI = (email, password) => {
+      return axios.post("http://localhost:5236/api/Authentication/Login", {
+        email,
+        password,
+      });
+    };
 
-      let response = await loginAPI(email, password);
-      if (response && response.token) {
-        localStorage.setItem("token", response.token);
-      }
-      console.log(">>> check login: ", response);
-
-      // const response = (email, password) => {
-      //   return axios.post("http://localhost:5236/api/Authentication/Login", {
-      //     email,
-      //     password,
-      //   });
-      // };
-
-      // console.log(response.data);
-      // localStorage.setItem("token", response.data.token);
-
-      onLogin(email);
-    } catch (error) {
-      setError(
-        "Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin đăng nhập."
-      );
-      console.error("Đăng nhập không thành công:", error);
+    let response = await loginAPI(email, password);
+    if (response && response.token) {
+      localStorage.setItem("token", response.token);
     }
+    console.log(">>> check login: ", response);
+
+    // const response = (email, password) => {
+    //   return axios.post("http://localhost:5236/api/Authentication/Login", {
+    //     email,
+    //     password,
+    //   });
+    // };
+
+    // console.log(response.data);
+    // localStorage.setItem("token", response.data.token);
+
+    // onLogin(email);
   };
+  //   catch (error) {
+  //     setError(
+  //       "Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin đăng nhập."
+  //     );
+  //     console.error("Đăng nhập không thành công:", error);
+  //   }
+  // };
 
   return (
     <div className="login-container">
@@ -83,7 +85,7 @@ const Login = ({ onLogin }) => {
           <div className="forgot-password">
             <a href="/forgotpass">Quên mật khẩu?</a>
           </div>
-          {error && <p className="error-message">{error}</p>}
+          {/* {error && <p className="error-message">{error}</p>} */}
           <button type="submit" onClick={() => handleSubmit()}>
             Đăng nhập
           </button>
